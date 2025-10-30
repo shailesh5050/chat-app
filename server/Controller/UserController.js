90fe0753d9e1318e767befd6da5520e30efb6d6f
shailesh5050 <shaileshkuswaha1@gmail.com>
2025-10-30 17:35:13 +0530

    Implement user login functionality with JWT authentication

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -1,6 +1,6 @@
 import UserModel from "../Models/UserModel.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken"
 class UserController {
   async register(req, res) {
     try {
@@ -37,6 +37,38 @@ class UserController {
       return res.status(201).json({ message: "User created successfully." });
     } catch (error) {
       console.error(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+
+      const user = await UserModel.findOne({ email })
+
+       // Compare password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+
+      const token = jwt.sign({ user }, process.env.SECRET)
+      res.cookie('token', token, {
+        httpOnly: true,    // JS can't read it — safer
+        secure: false,     // true in production (HTTPS)
+        sameSite: 'lax',   // helps prevent CSRF
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      })
+      return res.status(200).json({ data: { emai: user.email, id: user._id }, message: "Login Successfully" });
+
+
+
+    } catch (error) {
       return res.status(500).json({ message: "Internal Server Error" });
     }
   }
--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -3,5 +3,6 @@ import UserController from '../Controller/UserController.js'
 const userRoutes = express.Router()
 
 userRoutes.post('/register',UserController.register)
+userRoutes.post('/login',UserController.login)
 
 export default userRoutes
